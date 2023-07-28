37b3f4e40ca91caa74619ca0fe6ff668983ed709
Favionata16 <98546416+Favionata16@users.noreply.github.com>
2023-07-28 16:58:33 -0600

    se trabaja en el segundo ejercicio 2

--- Lab1Progra3FavioNataren22141210/main.cpp
+++ Lab1Progra3FavioNataren22141210/main.cpp
@@ -1,30 +1,55 @@
 using namespace std;
 #include <iostream>
 #include <vector>
+#include <cmath> // Para utilizar la función pow()
 
+double raizNesima(double x, int n2, double tolerancia = 1e-6, int maxIteraciones = 100) {
+	double aproximacion = 1.0; // Valor inicial de la aproximación
+	int ite = 0;//para contar las iteraciones
+
+	for (int i = 0; i < maxIteraciones; ++i) {
+		// Método de Newton-Raphson para calcular la aproximación
+		double nuevaAproximacion = ((n2 - 1.0) * aproximacion + x / pow(aproximacion, n2 - 1)) / n2;
+
+		// Verificar la convergencia
+		if (abs(nuevaAproximacion - aproximacion) < tolerancia) {
+			return nuevaAproximacion;
+		}
+
+		aproximacion = nuevaAproximacion;
+		ite++;
+		
+
+	}
+	
+
+	// Si no converge en el número máximo de iteraciones, devolver la última aproximación
+	return aproximacion;
+	
+}
 
 int main() {
 
 	int opcion = 0;
 
 	while (opcion != 3) {
-		cout << "\n----Menu----";
+		cout << "\n-----Menu-----";
 		cout << "\n1.Ejercicio 1";
 		cout << "\n2.Ejercicio 2";
-		cout << "\n3.salir";
+		cout << "\n3.Salir";
 		cout << "\nIngrese Opcion: ";
 		cin >> opcion;
 
 		switch (opcion) {// se usa un switch para hacer el menu
 
 		case 1: {
-			cout << "\nEjercicio 1";
+			cout << "\n--Ejercicio 1--";
 
 			int n;
 			cout << "Ingrese el tamaño del tablero (n x n): ";
 			cin >> n;
 
-			vector<vector<char>> tablero(n, vector<char>(n, ' ')); // se usa vectores para este ejercicio
+			vector<vector<char>> tablero(n, vector<char>(n, ' ')); // se usa un vector de characteres para este ejercicio
 
 			int fila = 0;
 			while (fila < n) {
@@ -58,9 +83,9 @@ int main() {
 			break;
 		}
 		case 2: {
-			cout << "\nEjercicio 2";
+			cout << "\n--Ejercicio 2--";
 
-			cout << "\n";
+
 			break;
 		}
 
